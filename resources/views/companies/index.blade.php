c5f50799ee6a6f52d81b8a39fc5340a54fcee442
ravideorwine <101636600+ravideorwine@users.noreply.github.com>
2022-03-16 14:43:48 +0530

    second commit Message

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -71,7 +71,6 @@
         <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
         <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
 
-
         <script type="text/javascript">
             $(function () {                
                 
